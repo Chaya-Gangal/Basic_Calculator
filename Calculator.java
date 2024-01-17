fd44468bf94166418302b18ae25385a5e7eb9cff
Chaya <01fe22bca176@kletech.ac.in>
2024-01-17 17:00:56 +0530

    Code for Basic Airthmatic Ops

---
+++ Calculator.java
@@ -0,0 +1,32 @@
+class Calculator{
+    float addition(float operand_1,float operand_2){
+    return (operand_1+operand_2);
+    }
+    float substraction(float operand_1,float operand_2){
+    return (operand_1-operand_2);
+    }
+    float multiplication(float operand_1,float operand_2){
+    return (operand_1*operand_2);
+    }
+    float division(float operand_1,float operand_2){
+    return (operand_1/operand_2);
+    }
+
+    public static void main(String[] args){
+        Calculator calc=new Calculator();
+        float operand_1=10;
+        float operand_2=5;
+
+        float add_result=calc.addition(operand_1, operand_2);
+        System.out.println("result is : "+add_result);
+
+        float sub_result=calc.substraction(operand_1, operand_2);
+        System.out.println("result is : "+sub_result);
+
+        float mult_result=calc.multiplication(operand_1, operand_2);
+        System.out.println("result is : "+mult_result);
+
+        float div_result=calc.division(operand_1, operand_2);
+        System.out.println("result is : "+div_result);
+    }
+}
